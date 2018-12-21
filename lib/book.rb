de7c69085ddc8d78cf5628fdc5dcb7706aab31f6
Kyle  Beckstead <kyle21beckstead@gmail.com>
2018-12-21 19:25:50 +0000

    Automatically backed up by Learn

---
+++ lib/book.rb
@@ -0,0 +1,22 @@
+# book.rb
+
+class Book
+  attr_accessor :author, :page_count  # remove the attr_accessor for genre
+  attr_reader :title, :genre  # add an attr_reader for genre
+
+  GENRES = []
+
+  def initialize(title)
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+ 
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
+end
